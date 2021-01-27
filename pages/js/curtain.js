a9d3689d93b92d758950aaee37de3d0103da55e9
Ahegan <enzosimioni.eu@gmail.com>
2021-01-27 16:28:32 +0100

    progress bar on articles

--- pages/js/curtain.js
+++ pages/js/curtain.js
@@ -18,7 +18,24 @@ function rotate() {
     document.getElementById("rot").style.transform = "rotate(7turn)"
 }
 
-var d = new Date();
+//whenever someone try to copy something on the page, it is remplaced by this message
+document.addEventListener('copy', (e) => {
+    e.clipboardData.setData('text/plain', 'How does it feel to steal other people\'s work ?');
+    e.preventDefault();
+});
+
+//progress shadow under the navBar
+
+
+window.addEventListener("scroll", () => {
+    var variationRate = window.scrollY < 400 ? 0 : window.scrollY < 550 ? 1 : window.scrollY < 700 ? 2 : 4;
+    variationRate = window.innerWidth < 520 && window.scrollY > 600 ? 5.5 : variationRate;
+    var advancement = window.scrollY;
+    var totalHeight = document.body.offsetHeight - window.innerHeight;
+    var calculOnPercent = Math.floor((advancement * 100) / totalHeight);
+    document.styleSheets[1].addRule(".menu::before", `width: ${calculOnPercent + variationRate}%`)
+})
+
 
 
 
